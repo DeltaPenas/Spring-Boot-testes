a62a8dab8381aeaaf4f4b8c744947bf81719845d
João Nemezio <joaogabrieldl30@gmail.com>
2026-01-19 19:18:17 -0300

    Primeiro commit, amanhã faço o controller

---
+++ meu-projeto-spring-boot/src/main/java/com/produtoapi/model/Produto.java
@@ -0,0 +1,39 @@
+package com.produtoapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+public class Produto {
+    @Setter
+    @Getter
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @NotEmpty(message = "Informe um nome.")
+    private String nome;
+    private int quantidade;
+    private double valor;
+    private String status;
+
+
+    public Produto(String nome, int quantidade, double valor, String status){
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.valor = valor;
+        this.status = status;
+
+    }
+
+    public Produto(){
+        //construtor vazio
+    }
+
+}
