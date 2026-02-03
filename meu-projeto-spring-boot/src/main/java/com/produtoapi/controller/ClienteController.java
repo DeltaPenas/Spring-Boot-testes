dea2c94333e133cfb5664ac9b9f5b41da40b232f
João Nemezio <joaogabrieldl30@gmail.com>
2026-02-03 16:56:16 -0300

    controller do cliente

--- meu-projeto-spring-boot/src/main/java/com/produtoapi/controller/ClienteController.java
+++ meu-projeto-spring-boot/src/main/java/com/produtoapi/controller/ClienteController.java
@@ -1,12 +1,11 @@
 package com.produtoapi.controller;
 
 
-import com.produtoapi.service.ProdutoService;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.produtoapi.model.Cliente;
 import com.produtoapi.service.ClienteService;
-import com.produtoapi.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -20,5 +19,26 @@ public class ClienteController {
     @Autowired
     private ClienteService clienteService;
 
+    @GetMapping
+    public List<Cliente> listarTodos(){
+        return clienteService.listarTodos();
+    }
+
+    @PostMapping
+    public Cliente salvar(@RequestBody Cliente cliente){
+        return clienteService.salvar(cliente);
+    }
+    @PutMapping("/{id}")
+    public Cliente atualizar(@PathVariable Long id, @RequestBody Cliente cliente){
+        return  clienteService.atualizar(id, cliente);
+    }
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id){
+        clienteService.deletar(id);
+    }
+    @GetMapping("/{id}")
+    public Optional<Cliente> findByid(@PathVariable Long id){
+        return clienteService.findById(id);
+    }
 
 }
